ac5877b0a7d1ad9b2a8371baf0f013cfecf88e5e
xieofxie <xieofxie@126.com>
2020-09-28 21:22:37 +0800

    add /testplugin/:projectId/test

--- Composer/plugins/webRoute/src/index.tsx
+++ Composer/plugins/webRoute/src/index.tsx
@@ -2,6 +2,8 @@
 // Licensed under the MIT License.
 
 import axios from 'axios';
+import { execSync } from 'child_process';
+
 import path = require('path');
 import globby = require('globby');
 
@@ -19,30 +21,85 @@ module.exports = {
       next();
     });
 
+    const getInfoAsync = async (projectId) => {
+      const projectInfo = await httpClient.get(`/projects/${projectId}`);
+      if(!!!projectInfo.data){
+        throw {message: 'Project does not exist!'};
+      }
+      const location = path.normalize(projectInfo.data.location);
+
+      let testFolder = projectInfo.data.settings.testFolder;
+      if(!!!testFolder){
+        throw {message: 'Set a testFolder in your settings!'};
+      }
+      testFolder = path.normalize(testFolder);
+
+      let runtime = projectInfo.data.settings.runtime?.path;
+      if(!!!runtime){
+        throw {message: 'Use special custom runtime for testing!'};
+      }
+      runtime = path.normalize(runtime);
+
+      return {location, testFolder, runtime};
+    };
+
+    // Output:
+    // location: string
+    // testFolder: string -> settings.testFolder
+    // runtime: string -> settings.runtime.path
+    composer.addWebRoute('get', '/testplugin/:projectId/info', async (req, res) => {
+      try{
+        res.send(await getInfoAsync(req.params.projectId));
+      }
+      catch(error){
+        res.send({error: error.message});
+      }
+    });
+
+    // Output:
     // error: string
     // testFolder: string -> settings.testFolder
     // testFiles: string list
     composer.addWebRoute('get', '/testplugin/:projectId/files', async (req, res) => {
-      const projectInfo = await httpClient.get(`/projects/${req.params.projectId}`);
-      if(!!!projectInfo.data){
-        res.send({error:'Project does not exist!'});
-        return;
+      try{
+        const info = await getInfoAsync(req.params.projectId);
+        let testFiles = [];
+        const paths = await globby(['**/*.dialog'], {cwd: info.testFolder});
+        for (const filePath of paths) {
+          const realFilePath: string = path.join(info.testFolder, filePath);
+          testFiles.push(realFilePath);
+        }
+        res.send({testFolder: info.testFolder, testFiles})
       }
+      catch(error){
+        res.send({error: error.message});
+      }
+    });
 
-      let testFolder = projectInfo.data.settings.testFolder;
-      if(!!!testFolder){
-        res.send({error:'Set a testFolder in your settings!'});
-        return;
-      }
+    // Input:
+    // testFile: string
+    // Output:
+    // result: string
+    composer.addWebRoute('post', '/testplugin/:projectId/test', async (req, res) => {
+      try{
+        const info = await getInfoAsync(req.params.projectId);
+        // TODO some workaround
+        const Tester = path.join(info.runtime, 'declarative_ut/bin/Debug/netcoreapp3.1/DeclarativeUT.exe');
 
-      testFolder = path.normalize(testFolder);
-      let testFiles = [];
-      const paths = await globby(['**/*.dialog'], {cwd: testFolder});
-      for (const filePath of paths) {
-        const realFilePath: string = path.join(testFolder, filePath);
-        testFiles.push(realFilePath);
+        let cmd = `${Tester} --autoDetect true --botFolder ${info.location} --testFolder ${info.testFolder} --testSubFolder ${req.body.testFile}`;
+        console.log(cmd);
+
+        try {
+          const output = execSync(cmd, { encoding: 'utf8' });
+          cmd = output;
+        } catch (error) {
+          cmd = error.toString();
+        }
+        res.send({result: cmd});
       }
-      res.send({testFolder, testFiles})
+      catch(error){
+        res.send({error: error.message});
+      }
     });
   },
 };
