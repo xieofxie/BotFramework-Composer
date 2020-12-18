d9d34a603dba93ee9004774708d3f7332dd1bc86
xieofxie <xieofxie@126.com>
2020-12-18 17:02:15 +0800

    check 1st, get resource later

--- Composer/chromeextension/src/contentscript/contentscript.tsx
+++ Composer/chromeextension/src/contentscript/contentscript.tsx
@@ -8,15 +8,49 @@ import { SimpleGet } from '../utilities/utilities';
 import TriggersRenderer from './TriggersRenderer';
 
 const schemaUrl = 'https://raw.githubusercontent.com/microsoft/BotFramework-Composer/main/Composer/packages/server/schemas/sdk.schema';
-let schema: any = {};
+let schema: any = null;
 
 const uischemaUrl = 'https://raw.githubusercontent.com/microsoft/BotFramework-Composer/main/Composer/packages/server/schemas/sdk.uischema';
-let uischema: any = {};
+let uischema: any = null;
 
-let pluginConfig: any = {};
+let pluginConfig: any = null;
+
+const GetSchemaAsync = async () => {
+    if (!schema) {
+        return await SimpleGet(schemaUrl)
+        .then((text: string) => {
+            schema = JSON.parse(text);
+            return schema;
+        });
+    }
+    return schema;
+};
+
+const GetUiSchemaAsync = async () => {
+    if (!uischema) {
+        return await SimpleGet(uischemaUrl)
+        .then((text: string) => {
+            uischema = JSON.parse(text);
+            return uischema;
+        });
+    }
+    return uischema;
+};
+
+const GetPluginConfigAsync = async () => {
+    if (!pluginConfig) {
+        return await GetUiSchemaAsync()
+        .then((uischema: any) => {
+            // Composer\packages\client\src\pages\design\DesignPage.tsx
+            pluginConfig = mergePluginConfigs({ uiSchema: uischema }, plugins);
+            return pluginConfig;
+        })
+    }
+    return pluginConfig;
+};
 
 // return new elem
-function ConfigureShowHide(originalElem, id){
+const ConfigureShowHide = (originalElem, id) => {
     console.error(`ConfigureShowHide: ${id}`);
     originalElem.hide();
     var button = `<button id=${'button_'+id}>Toggle</button>`;
@@ -32,33 +66,25 @@ function ConfigureShowHide(originalElem, id){
     return divElem;
 }
 
-function Render(content, rootElem){
+const RenderAsync = async (content, rootElem) => {
     var data = JSON.parse(content);
-    var elem = <TriggersRenderer schema={schema} plugins={pluginConfig} data={data}></TriggersRenderer>;
+    var elem = <TriggersRenderer schema={await GetSchemaAsync()} plugins={await GetPluginConfigAsync()} data={data}></TriggersRenderer>;
     ReactDOM.render(elem, rootElem[0]);
 }
 
-function HandleRawAsync(){
+const HandleRawAsync = async () => {
     var rawLinkElem = $('a#raw-url');
     if(rawLinkElem.length == 0) return;
     var url = rawLinkElem.attr('href');
     if(!url.endsWith('.dialog')) return;
-    return SimpleGet(url)
-    .then((text: string) => {
+    return await SimpleGet(url)
+    .then(async (text: string) => {
         var bodyElem = $('div.Box-body[itemprop="text"]');
         var renderElem = ConfigureShowHide(bodyElem, 'renderraw');
-        Render(text, renderElem);
+        await RenderAsync(text, renderElem);
     });
 }
 
 $(async ()=>{
-    await SimpleGet(uischemaUrl)
-    .then((text: string) => {
-        uischema = JSON.parse(text);
-        // Composer\packages\client\src\pages\design\DesignPage.tsx
-        pluginConfig = mergePluginConfigs({ uiSchema: uischema }, plugins);
-    })
-    .then(() => {
-        return HandleRawAsync();
-    });
+    return await HandleRawAsync();
 });
